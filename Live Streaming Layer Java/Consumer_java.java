7701ff0e27085811417a32cc2e0828eb8679da70
dpantelaios <94012055+dpantelaios@users.noreply.github.com>
2023-02-17 21:56:19 +0200

    Add files via upload

--- Live Streaming Layer Java/Consumer_java.java
+++ Live Streaming Layer Java/Consumer_java.java
@@ -10,18 +10,18 @@ public class Consumer_java {
 	public static void main(String[] args) {
 		//create kafka consumer
 		Properties properties = new Properties();
-		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29090");
 		properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
-		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		// properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		// properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-		Consumer<String, String> consumer = new KafkaConsumer<>(properties);
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 		//subscribe to topic
 		consumer.subscribe(Collections.singleton("th1"));
 		//poll the record from the topic
 		while (true) {
-			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+			ConsumerRecords<String, String> records = consumer.poll(500);
 			for (ConsumerRecord<String, String> record : records) {
 				System.out.println("Message received: " + record.value());
 			}
